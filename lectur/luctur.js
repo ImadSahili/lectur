7734ce6140e1aaaf39d1267704e03f4a4337aa7b
imad sahili <123188547+ImadSahili@users.noreply.github.com>
2023-06-23 20:56:43 +0100

    Add files via upload

--- lectur/luctur.js
+++ lectur/luctur.js
@@ -1,32 +1,41 @@
 const textarea = document.querySelector("textarea");
 const btnSpeck = document.querySelector("i");
+const btnMacro = document.querySelector("e");
 
 btnSpeck.addEventListener("click", () => {
   const textSpeck = new SpeechSynthesisUtterance(textarea.value);
   speechSynthesis.speak(textSpeck);
 });
 
+function startSpeechRecognition() {
+  // التحقق مما إذا كان المتصفح يدعم واجهة التعرف على الكلام
+  if ("webkitSpeechRecognition" in window) {
+    // إنشاء كائن لواجهة التعرف على الكلام
+    var recognition = new webkitSpeechRecognition();
 
+    // تعيين إعدادات لواجهة التعرف على الكلام
+    recognition.lang = "en"; // تحديد اللغة للتعرف على الكلام (بالعربية)
+    recognition.continuous = false; // التعرف على الكلام المستمر (بدون توقف)
 
-// التحقق مما إذا كان المتصفح يدعم واجهة التعرف على الكلام
-if ('webkitSpeechRecognition' in window) {
-  // إنشاء كائن لواجهة التعرف على الكلام
-  var recognition = new webkitSpeechRecognition();
+    // تنفيذ عندما يتم الكشف عن كلام
+    recognition.onresult = function (event) {
+      var result = event.results[0][0].transcript; // الحصول على النص المحول
 
-  // تعيين إعدادات لواجهة التعرف على الكلام
-  recognition.lang = 'ar'; // تحديد اللغة للتعرف على الكلام (بالعربية)
-  recognition.continuous =false; // التعرف على الكلام المستمر (بدون توقف)
+      // قم بتنفيذ الإجراءات المطلوبة مع النص
+      textarea.value = result;
+      console.log("النص المحول:", result);
+    };
 
-  // تنفيذ عندما يتم الكشف عن كلام
-  recognition.onresult = function(event) {
-    var result = event.results[0][0].transcript; // الحصول على النص المحول
-
-    // قم بتنفيذ الإجراءات المطلوبة مع النص
-    console.log('النص المحول:', result);
-  };
-
-  // بدء عملية التعرف على الكلام
-  recognition.start();
-} else {
-  console.log('عذرًا، واجهة التعرف على الكلام غير مدعومة في هذا المتصفح.');
+    // بدء عملية التعرف على الكلام
+    recognition.start();
+    // recognition.onend=function(){
+    //   console.log("s")
+    // }
+  } else {
+    console.log("عذرًا، واجهة التعرف على الكلام غير مدعومة في هذا المتصفح.");
+  }
 }
+
+btnMacro.addEventListener("click", () => {
+  startSpeechRecognition();
+});
